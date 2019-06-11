94d4b8940394eccc370afc14219e76bdcc61a56b
donna <sheehanqmd@gmail.com>
2019-06-11 15:51:55 -0500

    complete

--- closures.js
+++ closures.js
@@ -23,12 +23,14 @@ function outer() {
 */
   
 // Code Here
+var inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+console.log(inner);
 
 
 
@@ -52,6 +54,8 @@ function callFriend(name) {
 */
 
 //Code Here
+var callJake = callFriend("Jake")
+callJake(435-555-9248)
 
 
 
@@ -62,15 +66,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  var result = 0
+  function counter(){
+    return result += 1;
+  }
+  return counter
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -89,15 +99,20 @@ function counterFactory(value) {
   // Code here.
 
   return {
-
+     inc: function (){
+       return value += 1;
+     },
+     dec: function(){
+       return value -= 1;
+     }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +128,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message(){ 
+    return welcomeText + " " + firstname + " " + lastname + ".";
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +161,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 })();
 
@@ -163,8 +183,24 @@ function secretNumber() {
 
   return {
     // Code here
-  };
+    addToSecret: function(num) {
+      secret += num 
+      
+      return secret
+    },
+
+    takeAwayFromSecret: function(num) {
+      secret -= num
+      return secret
+    }
+  }
 }
+
+// var secret = secretNumber()
+// secret.addToSecret(10);
+
+// var secret = secretNumber()
+// secret.takeAwayFromSecret(10)
 
 
 
